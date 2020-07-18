e7327c1abe7a8a00662bf4cd154d56dc946df94e
[synnaf] <[f.vforsman@gmail.com]>
2020-07-18 20:23:23 +0200

    Creatung a counter component and setting up dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
+import Counter from './components/Counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+     
+    <Counter></Counter>
+
     </div>
   );
 }
---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import './Counter.scss';
+
+// export class HelloWorldProps {
+//     message: string = ''; 
+// }
+
+function HelloWorld() {
+    return (
+
+        <h2>Hello worldaaaa</h2>
+
+ 
+
+    ); 
+}
+
+export default HelloWorld; 
